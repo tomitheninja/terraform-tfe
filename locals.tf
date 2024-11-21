8156a19134222fd0f1fd91a360553da12c119eaa
Südi Tamás <57364372+tomitheninja@users.noreply.github.com>
2024-11-21 02:27:47 +0100

    dummy change

--- locals.tf
+++ locals.tf
@@ -6,7 +6,7 @@ locals {
   }
   workspace = {
     "terraform-tfe" = {
-      description         = "Automation for Terraform Enterprise using GitHub"
+      description         = "Automation for Terraform Enterprise using GitHub 789"
       execution_mode      = "remote"
       project_id          = module.project["tf-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/${var.github_tfe_repository_name}"
